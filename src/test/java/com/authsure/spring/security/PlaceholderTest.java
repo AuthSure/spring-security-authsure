19dd1361ca83c978760cf1f9ec2fe75d3cf02df6
Michael Dollar <mdollar@netradius.com>
2017-02-01 15:45:41 -0700

    AS-19 - adding a Placeholder Test

---
+++ src/test/java/com/authsure/spring/security/PlaceholderTest.java
@@ -0,0 +1,11 @@
+package com.authsure.spring.security;
+
+import org.junit.Test;
+
+/**
+ * Created by mdollar on 2/1/17.
+ */
+public class PlaceholderTest {
+    @Test
+    public void test(){}
+}
